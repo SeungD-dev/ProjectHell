81dff1bf7d640dd94844cc7295897429558eef07
NellySach <dldbstn147@naver.com>
2023-01-02 21:49:52 +0900

    1-1

--- Assets/Scripts/Stage1/MovementObject1_1.cs
+++ Assets/Scripts/Stage1/MovementObject1_1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementObject : MonoBehaviour
+public class MovementObject1_1 : MonoBehaviour
 {
     [SerializeField]
     private VirtualJoystick virtualJoystick;
@@ -14,22 +14,22 @@ public class MovementObject : MonoBehaviour
         float x = virtualJoystick.Horizontal();
         float y = virtualJoystick.Vertical();
 
-        if ( x != 0 || y != 0)
+        if (x != 0 || y != 0)
         {
             transform.position += new Vector3(-y, 0, x) * moveSpeed * Time.deltaTime;
         }
         if (ViewCamera != null)
         {
-            Vector3 direction = (Vector3.up * 4 + Vector3.back) * 2;
+            Vector3 direction = new Vector3(6, 9, 0);
             RaycastHit hit;
             Debug.DrawLine(transform.position, transform.position + direction, Color.red);
             if (Physics.Linecast(transform.position, transform.position + direction, out hit))
             {
-                ViewCamera.transform.position = hit.point;
+                ViewCamera.transform.position = hit.point + new Vector3(0, 1, 0);
             }
             else
             {
-                ViewCamera.transform.position = transform.position + new Vector3(6, 9, 0);
+                ViewCamera.transform.position = transform.position + direction;
             }
             ViewCamera.transform.LookAt(transform.position);
             ViewCamera.transform.eulerAngles = new Vector3(45, -90, 0);
