08ff32eb9f99ab0c42a2809ed6d1362a4ec3c567
201870024YuSeungHyeon <jyyootom@naver.com>
2023-03-18 21:48:00 +0900

    아누비스 애니메이션 버그 수정, FX

---
+++ Assets/Scripts/Stage1/BoomFX.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class BoomFX : MonoBehaviour
+{
+    public Animator anim;
+    GameObject boom;
+    SpriteRenderer sr;
+    float time;
+    bool startFX;
+
+    void Start()
+    {
+        anim = gameObject.GetComponentInChildren<Animator>();
+        boom = GameObject.Find("boom");
+        sr = boom.GetComponent<SpriteRenderer>();
+        sr.color = new Color(255, 255, 255, 255);
+        startFX = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 23)
+        {
+            startFX = true;
+        }
+        if(time >= 24.9 && startFX == true)
+        {
+            anim.SetTrigger("bang");
+            startFX = false;
+            time = 0;
+           
+        }
+    }
+}
