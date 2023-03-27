81834b231026feacb406b9687d414a228134042f
NellySach <dldbstn147@naver.com>
2023-03-27 23:46:00 +0900

    노트 이미지 실험

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -145,7 +145,7 @@ public class PlayerControl : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Note_R") || other.CompareTag("Note_G") || other.CompareTag("Note_B") || other.CompareTag("Note_X") ||
-            other.CompareTag("SwordTrail") || other.CompareTag("SwordTrail_Vertical"))
+            other.CompareTag("SwordTrail") || other.CompareTag("SwordTrail_Vertical") || other.CompareTag("Note_BB") || other.CompareTag("Note_BG") || other.CompareTag("Note_BR"))
         {
             Destroy(other.gameObject);
             if (!isHurt)
